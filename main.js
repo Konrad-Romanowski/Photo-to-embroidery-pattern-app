5ad64f63a495bfa4aefe1345bbb040dff0669a32
Konrad Romanowski <konrad.romanowski.it@gmail.com>
2022-02-13 18:02:49 +0100

    Added image preview functionality

--- main.js
+++ main.js
@@ -8,4 +8,52 @@ document.addEventListener('scroll', ()=>{
     } else {
         navbar.classList.remove('scrolled');
     }
-})
+});
+
+const fileInput = document.getElementById('image-input');
+
+fileInput.addEventListener('change', e => {
+    const file = fileInput.files[0];
+    const imageType = /image.*/;
+    
+    const previevSection = document.getElementById('previev-section');
+    const errorMessageSection = document.getElementById('error-message');
+
+    if (file.type.match(imageType)) {
+        const reader = new FileReader();
+
+        const imageNameContainer = document.getElementById('image-name');
+        const imageResolutionContainer = document.getElementById('image-resolution');
+        const stichSizeValueSpan = document.getElementById('stich-size-value');
+        const stichSizeSlider = document.getElementById('stich-size')
+        const previewImageContainer = document.getElementById('preview-image');
+        imageNameContainer.innerText = file.name;
+
+        stichSizeSlider.value = 12;
+        stichSizeValueSpan.innerText = stichSizeSlider.value;
+
+        stichSizeSlider.addEventListener('input', e => {
+            stichSizeValueSpan.innerText = e.target.value;
+        });
+
+        reader.onload = e => {
+            const img = new Image();
+            img.src = reader.result;
+            img.onload = () =>{
+                previewImageContainer.src = img.src;
+                imageResolutionContainer.innerText = `${img.width} x ${img.height}`;
+            }
+        }
+
+        reader.readAsDataURL(file);
+        errorMessageSection.style.display = 'none';
+        previevSection.style.display = 'block';
+        previevSection.scrollIntoView();
+    } else {
+        // Case when file not supported!
+
+        previevSection.style.display = 'none';
+        errorMessageSection.style.display = 'block';
+        errorMessageSection.scrollIntoView();
+    }
+});
